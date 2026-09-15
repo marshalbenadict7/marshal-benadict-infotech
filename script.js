6086da32d777c16210a0bba69ca7d4624e004159
marshalbenadict7 <marshalbenadict7@gmail.com>
2026-09-15 14:48:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Contact Form
     const contactForm = document.getElementById("contactForm");
 
-    const successMessage =
-        document.getElementById("successMessage");
+    contactForm.addEventListener("submit", function (event) {
 
+        event.preventDefault();
 
-    if (contactForm) {
+        alert("JavaScript is working!");
 
-        contactForm.addEventListener("submit", function (event) {
-
-            // Stop form from refreshing
-            event.preventDefault();
-
-
-            // Get input values
-
-            const name =
-                document.getElementById("name").value.trim();
-
-            const email =
-                document.getElementById("email").value.trim();
-
-            const subject =
-                document.getElementById("subject").value.trim();
-
-            const message =
-                document.getElementById("message").value.trim();
-
-
-            // Check empty fields
-
-            if (
-                name === "" ||
-                email === "" ||
-                subject === "" ||
-                message === ""
-            ) {
-
-                alert("Please fill all fields.");
-
-                return;
-            }
-
-
-            // Email validation
-
-            const emailPattern =
-                /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-
-            if (!emailPattern.test(email)) {
-
-                alert("Please enter a valid email address.");
-
-                return;
-            }
-
-
-            // Show success message
-
-            successMessage.classList.remove("d-none");
-
-
-            successMessage.innerHTML =
-                "Thank you! Your message has been submitted successfully.";
-
-
-            // Clear form
-
-            contactForm.reset();
-
-        });
-
-    }
+    });
 
 });
